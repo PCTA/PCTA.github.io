1d977ba7ec402657d830ab6581b07641d349398e
PCTA <pctasteward@gmail.com>
2016-12-07 12:44:55 -0800

    Add files via upload

---
+++ ObjectFiles/chapters.js
@@ -0,0 +1,8 @@
+var chaptersJSON = {
+ "Reg03Ch01": {"chaptRegion": "Reg03", "chaptID":  "Ch01", "chaptName":  "none", "chaptContact":  "none", "begMile":  997.1, "endMile":  1017.0 }, 
+ "Reg03Ch02": {"chaptRegion": "Reg03", "chaptID":  "Ch02", "chaptName":  "Carsonora", "chaptContact":  "Dave Harrison", "begMile":  1017.0, "endMile":  1076.7 }, 
+ "Reg03Ch03": {"chaptRegion": "Reg03", "chaptID":  "Ch03", "chaptName":  "none", "chaptContact":  "none", "begMile":  1076.7, "endMile":  1079.7 }, 
+ "Reg03Ch04": {"chaptRegion": "Reg03", "chaptID":  "Ch04", "chaptName":  "TRTA", "chaptContact":  "Morgan", "begMile":  1079.7, "endMile":  1129.7 }, 
+ "Reg03Ch05": {"chaptRegion": "Reg03", "chaptID":  "Ch05", "chaptName":  "none", "chaptContact":  "none", "begMile":  1129.7, "endMile":  1183.9 }, 
+ "Reg03Ch06": {"chaptRegion": "Reg03", "chaptID":  "Ch06", "chaptName":  "Pounders Promise", "chaptContact":  "Charles Williams", "begMile":  1183.9, "endMile":  1417.0 } 
+} ;
---
+++ ObjectFiles/regions.js
@@ -0,0 +1,8 @@
+var regionsJSON = {
+ "Reg01": {"regID": "Reg01", "regName":  "Southern California", "regRep":  "Anitra Kass", "begMile":  0.0, "endMile":  652.0 }, 
+ "Reg02": {"regID": "Reg02", "regName":  "Southern Sierra", "regRep":  "vacant", "begMile":  652.0, "endMile":  997.1 }, 
+ "Reg03": {"regID": "Reg03", "regName":  "Northern California", "regRep":  "Justin Kooyman", "begMile":  997.1, "endMile":  1417.0 }, 
+ "Reg04": {"regID": "Reg04", "regName":  "Big Bend", "regRep":  "Ian Nelson", "begMile":  1417.0, "endMile":  1875.8 }, 
+ "Reg05": {"regID": "Reg05", "regName":  "Columbia Cascades", "regRep":  "Dana Hendricks", "begMile":  1875.8, "endMile":  2292.4 }, 
+ "Reg06": {"regID": "Reg06", "regName":  "North Cascades", "regRep":  "Bill Hawley", "begMile":  2292.4, "endMile":  2658.9 } 
+} ;
